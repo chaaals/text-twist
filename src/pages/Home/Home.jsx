9d9ceddc19c2559fb3fe87af86e49cd0d6ffd807
chaaals <charlesching0808@gmail.com>
2023-06-07 08:02:02 +0800

    refactor: home indentation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
-import AppContext from '../../context/app.context';
-import './Home.css';
+import { useContext } from "react";
+import AppContext from "../../context/app.context";
+import "./Home.css";
 
 // import db from "../../firebase/firebase-config";
 // import { collection, addDoc } from "@firebase/firestore";
@@ -11,43 +11,43 @@ import './Home.css';
 //   LevelFourData,
 //   LevelFiveData,
 // } from "../../data";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Home() {
-	// == TODO: Remove this when the app is ready, only use this when we add data to db
-	// const levelData = {
-	//   1: LevelOneData,
-	//   2: LevelTwoData,
-	//   3: LevelThreeData,
-	//   4: LevelFourData,
-	//   5: LevelFiveData,
-	// };
+  // == TODO: Remove this when the app is ready, only use this when we add data to db
+  // const levelData = {
+  //   1: LevelOneData,
+  //   2: LevelTwoData,
+  //   3: LevelThreeData,
+  //   4: LevelFourData,
+  //   5: LevelFiveData,
+  // };
 
-	// const add = async () => {
-	//   for (let i = 1; i <= 5; i++) {
-	//     const levelCollectionRef = collection(db, `level-${i}`);
+  // const add = async () => {
+  //   for (let i = 1; i <= 5; i++) {
+  //     const levelCollectionRef = collection(db, `level-${i}`);
 
-	//     for (const data of levelData[i]) {
-	//       await addDoc(levelCollectionRef, data);
-	//     }
-	//   }
-	// };
+  //     for (const data of levelData[i]) {
+  //       await addDoc(levelCollectionRef, data);
+  //     }
+  //   }
+  // };
 
-	const { getLevelData } = useContext(AppContext);
+  const { getLevelData } = useContext(AppContext);
 
-	return (
-		<main>
-			<h1>Text Twist Game</h1>
+  return (
+    <main>
+      <h1>Text Twist Game</h1>
 
-			<button>
-				<Link to="/play" onClick={() => getLevelData()}>
-					Play
-				</Link>
-			</button>
+      <button>
+        <Link to="/play" onClick={() => getLevelData()}>
+          Play
+        </Link>
+      </button>
 
-			{/* <button onClick={add}>Add Data</button> */}
-		</main>
-	);
+      {/* <button onClick={add}>Add Data</button> */}
+    </main>
+  );
 }
 
 export default Home;
